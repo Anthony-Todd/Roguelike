a2c3af9d9a2fa9b6fe7223202ea153ccae51a3da
Anthony-Todd <toddy201@gmail.com>
2012-09-10 11:38:40 +0100

    Created Prototype debug menue for altering Config file Debug menue bound to F1

--- src/Game/DebugMenu.py
+++ src/Game/DebugMenu.py
@@ -4,12 +4,59 @@ Created on Sep 9, 2012
 @author: anthony
 '''
 
-import cocos
+import cocos.layer
+from pyglet.window import key
+import Config
+import __builtin__
+import sys
+class Property( cocos.menu.EntryMenuItem ):
+    
+    def __init__(self, obj, key):
+        self.key = key
+        self.obj = obj
+        value = self.obj.__dict__[self.key]
+        self.valueType = value.__class__.__name__
+        print self.valueType
+        super(Property, self).__init__(key+':', self.on_entry_callback, str(value), max_length=64)
+    
+    def on_entry_callback(self, value):
+        print value
+        try:
+            type_ = getattr(__builtin__,self.valueType )
+            typedValue = type_(value)
+            setattr(self.obj, self.key, typedValue);
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
 
-class PropertyMenue( cocos.menu.Menu):
+class PropertyMenu( cocos.menu.Menu ):
     
     def __init__( self, title=''):
-        super( PropertyMenue, self ).__init__(title)
+        super( PropertyMenu, self ).__init__(title)
+        self.menuItems = []
+        
+        self.AddObject(Config.Configuration, None)
+        
+        #item2= Property( Config.Configuration, 'PlayerSpeed')
+        #self.create_menu( [item1, item2] )
+        
+    def AddObject(self, obj, key=None):
+        if key is None:
+            for k in obj.__dict__.iterkeys():
+                if '__' in k:
+                    pass
+                else:
+                    self.menuItems.append( Property( obj, k ) )
+        self.create_menu( self.menuItems )
+                
+        
+class DebugMenuLayer(cocos.layer.Layer):
+    def __init__(self, gameScene):
+        self.game = gameScene
+        super(DebugMenuLayer, self).__init__()
+        self.is_event_handler = True
+    
+    def on_key_press( self, k , m ):
+        self.game.on_key_press(k,m)
 
 class DebugMenu(cocos.scene.Scene):
     '''
@@ -19,6 +66,12 @@ class DebugMenu(cocos.scene.Scene):
 
     def __init__( self ):
         super( DebugMenu, self ).__init__()
-        self.add( PropertyMenue('Debug Menue'), (0,640) )
-        
+        self.layer = DebugMenuLayer(self)
+        self.layer.add( PropertyMenu('Debug Menu'), (0,640) )
+        self.add( self.layer )
+    
+    def on_key_press( self, k , m ):
+        print k, m
+        if k is key.F1:
+            cocos.director.director.pop()
 debugMenu = DebugMenu()
--- src/Game/Game.py
+++ src/Game/Game.py
@@ -3,36 +3,46 @@ Created on Sep 2, 2012
 
 @author: anthony
 '''
-import cocos
+import cocos.layer
 import Level
 import UI
 import DebugMenu
-import pyglet
 from pyglet.window import key
+
+class GameLayer(cocos.layer.Layer):
+    def __init__(self, gameScene):
+        self.game = gameScene
+        super(GameLayer, self).__init__()
+        self.is_event_handler = True
+    
+    def on_key_press( self, k , m ):
+        self.game.on_key_press(k,m)
 
 class Game( cocos.scene.Scene ):
     '''
     classdocs
     '''
 
-
     def __init__( self, *argv, **kwargv ):
         '''
         Constructor
         '''
+        super(Game, self).__init__()
+        
         self.level = Level.Level(parent=self);
         self.ui    = UI.UI()
-        super(Game, self).__init__()
-        for l in [self.level, self.ui]:
+        self.gameLayer = GameLayer(self)
+        
+        for l in [self.level, self.ui,self.gameLayer]:
             self.add(l)
+        
         self.schedule(self.Update)
-        self.enable_handlers(True)
     
     def Update(self,dt):
         pass
     
     def on_key_press( self, k , m ):
         print k, m
-        if k == key.ENTER:
+        if k == key.F1:
             cocos.director.director.push(DebugMenu.debugMenu)
         
