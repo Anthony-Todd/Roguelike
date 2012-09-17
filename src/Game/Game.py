32a995bb5fa8055fdd13a831a092a4157579b920
marie <rieya.online@gmail.com>
2012-09-17 17:22:17 +0100

    playing with the collision system

--- src/Game/Game.py
+++ src/Game/Game.py
@@ -19,7 +19,7 @@ from cocos.sprite import Sprite
 import cocos.euclid as eu
 
 class CharController(actions.Action, tiles.RectMapCollider):
-    
+
     def step(self, dt):
         global keyboard, scroller, object_map
 
@@ -27,35 +27,43 @@ class CharController(actions.Action, tiles.RectMapCollider):
         dy = (keyboard[key.UP] - keyboard[key.DOWN]) * Configuration.PlayerSpeed * dt
 
         # get the player's current bounding rectangle
-        last = self.target.get_rect()
-        new = last.copy()
-        new.x += dx
-        new.y += dy
+        #last = self.target.get_rect()
+        #new = last.copy()
+        #new.x += dx
+        #new.y += dy
 
         # run the collider
+        (newx, newy) = (self.target.position[0] + dx, self.target.position[1] + dy)
+        cols = check_collisions(newx, newy, 16)
+        print len(cols)
+        #cols = check_collisions(self.target)
         #dx, dy = self.collide_map(object_map, last, new, dy, dx)
-        
-        if dx < 0.1 and dx > -0.1 and dy < 0.1 and dy > -0.1:
-            if self.target.image is not self.target.animations['idle_down']:
-                self.target.image = self.target.animations['idle_down']
-        elif dx > 0.0:
-            if self.target.image is not self.target.animations['walk_right']:
-                self.target.image = self.target.animations['walk_right']
-        elif dx < 0.0:
-            if self.target.image is not self.target.animations['walk_left']:
-                self.target.image = self.target.animations['walk_left']
-        elif dy > 0.0:
-            if self.target.image is not self.target.animations['walk_up']:
-                self.target.image = self.target.animations['walk_up']
-        elif dy < 0.0:
-            if self.target.image is not self.target.animations['walk_down']:
-                self.target.image = self.target.animations['walk_down']
-        
-        # player position is anchored in the center of the image rect
-        self.target.position = new.center
 
-        # move the scrolling view to center on the player
-        scroller.set_focus(*new.center)
+        if len(cols) == 0:
+            if dx < 0.1 and dx > -0.1 and dy < 0.1 and dy > -0.1:
+                if self.target.image is not self.target.animations['idle_down']:
+                    self.target.image = self.target.animations['idle_down']
+            elif dx > 0.0:
+                if self.target.image is not self.target.animations['walk_right']:
+                    self.target.image = self.target.animations['walk_right']
+            elif dx < 0.0:
+                if self.target.image is not self.target.animations['walk_left']:
+                    self.target.image = self.target.animations['walk_left']
+            elif dy > 0.0:
+                if self.target.image is not self.target.animations['walk_up']:
+                    self.target.image = self.target.animations['walk_up']
+            elif dy < 0.0:
+                if self.target.image is not self.target.animations['walk_down']:
+                    self.target.image = self.target.animations['walk_down']
+        
+            # player position is anchored in the center of the image rect
+            #self.target.position = new.center
+            #self.target.cshape.center = new.center
+            self.target.position = eu.Vector2(newx,newy)
+
+            # move the scrolling view to center on the player
+            #scroller.set_focus(*new.center)
+            scroller.set_focus(newx,newy)
 
 class CharacterSprite(Sprite):
 
@@ -66,23 +74,29 @@ class CharacterSprite(Sprite):
         super(CharacterSprite, self).__init__(animations[defaultAnimation], 
                 position, rotation, scale, opacity, color, anchor)
 
-class Character(layer.ScrollableLayer):
+class Character(cocos.cocosnode.CocosNode):
     
     def __init__(self, animationDict):
         super(Character, self).__init__()
         self.sprite = CharacterSprite(animationDict, 'idle_down')
-        self.sprite.position = (30,30)
+        self.position = (0,0)
+        #self.sprite.position = (30,30)
         self.add(self.sprite)
         self.sprite.do(CharController())
         self.sprite.cshape = cocos.collision_model.AARectShape(eu.Vector2(0.0, 0.0), 16, 16)
 
+cm = None
+def check_collisions(x,y,size):
+    global cm
+    return cm.objs_into_box(x-size,x+size,y-size,y+size)
 
 class Game(Scene):
     '''
     classdocs
     '''
 
-    def __init__(self, *argv, **kwargv):        
+    def __init__(self, *argv, **kwargv): 
+        
         super(Game, self).__init__()
         self.schedule(self.Update)
         
@@ -93,7 +107,7 @@ class Game(Scene):
         
         #print self.CharacterAnimations
         
-        global keyboard, scroller, object_map
+        global keyboard, scroller, object_map, cm
 
         keyboard = key.KeyStateHandler()
         director.window.push_handlers(keyboard)
@@ -102,7 +116,7 @@ class Game(Scene):
         
         ## add other layers here
         tilemap = cocos.tiles.load('../assets/level0.tmx')
-        layers = loadLevel.load_level(tilemap)
+        layers, cm = loadLevel.load_level(tilemap)
         
         topz=0
         for l in layers:
@@ -110,9 +124,11 @@ class Game(Scene):
             topz += 1
 
         self.player = Character(self.CharacterAnimations['george'])
-        scroller.add(self.player, z=topz)
+        #scroller.add(self.player, z=topz)
         
-        self.add(scroller)
+        self.add(scroller,z=0)
+        self.add(self.player, z=1)
+
     
     def on_key_press(self, k, m):
         if k is key.F1:
--- src/Game/loadLevel.py
+++ src/Game/loadLevel.py
@@ -5,40 +5,45 @@ import ast
 
 def load_level(tilemap):
     layers = []
-    
+    cm = cocos.collision_model.CollisionManagerGrid(0,500,0,500,16*1.25,16*1.25)
+
     for k in sorted(tilemap.__dict__['contents'].keys()):
         if 'background' in k:
             layers.append(tilemap[k])
 
         elif 'blocks' in k:
-            layers.append(collision_layer(tilemap[k]))
+            new_layer,cm = collision_layer(tilemap[k], cm)
+            layers.append(new_layer)
+    #print type(cm)
+    return layers, cm
 
-    return layers
+def collision_layer(tilelayer, cm, alt_tilelayer=None):
 
-def collision_layer(tilelayer, alt_tilelayer=None, layer=None):
-
-    def collision_sprite(img):
+    def collision_sprite(img, pos):
         sprite = cocos.sprite.Sprite(img)
-        sprite.cshape = cocos.collision_model.AARectShape(eu.Vector2(0.0,0.0),8,8)
+        sprite.cshape = cocos.collision_model.AARectShape(pos, 8,8)
+        sprite.position = pos
         return sprite
 
     #cm = cocos.collision_model.CollisionManagerGrid(0,100,0,100,32,32)
-    cm = cocos.collision_model.CollisionManager()
+    #cm = cocos.collision_model.CollisionManager()
     toggle_objs = {}
 
-    if layer == None:
-        layer = cocos.layer.ScrollableLayer()
-    
+    #if layer == None:
+    layer = cocos.layer.ScrollableLayer()
+    batch = cocos.batch.BatchNode()
+    layer.add(batch)
+        
     for col in tilelayer.cells:
         for cell in col:
             if cell.tile != None:
                 
                 img = cell.tile.image.get_image_data().texture
-                sprite = collision_sprite(img)
-                sprite.position = cell.center
+                sprite = collision_sprite(img, cell.center)
+                #sprite.position = cell.center
 
                 if cell.get('type') == 'block':
-                    layer.add(sprite)
+                    batch.add(sprite)
         
                 elif cell.get('type') == 'toggle':
                     sprite.norm = img
@@ -49,18 +54,18 @@ def collision_layer(tilelayer, alt_tilelayer=None, layer=None):
                     sprite.alt = cell.tile.image.get_image_data().texture
                     sprite.toggled = False
 
-                    layer.add(sprite)
+                    batch.add(sprite)
                     toggle_objs[sprite.position] = sprite
-
-                #cm.add(sprite)
+                
+                cm.add(sprite)
                                
-    return layer
+    return layer,cm
 '''
     def on_mouse_press(self, x, y, buttons, modifiers):
         cell = self.tilemap.get_at_pixel(x,y)
         
         if cell.center in self.objects:
-            pos = cell.center
+           pos = cell.center
             sprite = self.objects[pos]
 
             if sprite.toggled:
